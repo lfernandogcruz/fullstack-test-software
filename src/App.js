73b8568dc1ff98f79d4086c4aa7539223e67fd56
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-04-16 03:17:37 -0300

    feat: apply Board component on App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import QueryProvider from './context/QueryProvider';
 import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
   return (
     <QueryProvider>
       <div className="App">
         <Header />
+        <Board />
       </div>
     </QueryProvider>
   );
