7ff57d60edaa9ed973bbf8daf69337b8dafd5782
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-04-16 02:57:43 -0300

    feat: creates Header component with Select implemented

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Select from "./Select";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <Select type="source" />
+      <Select type="category" />
+    </header>
+  );
+}
+
+export default Header;
