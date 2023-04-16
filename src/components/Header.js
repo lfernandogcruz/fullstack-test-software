8d6f6712070dddd6997ffc71fd61a0a6802dc19e
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-04-16 03:16:21 -0300

    feat: presets button on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
+import { useContext } from "react";
+import QueryContext from "../context/QueryContext";
 import Select from "./Select";
 import TextInput from "./TextInput";
 
 function Header() {
+  const { source, category, search } = useContext(QueryContext);
+
   return (
     <header className="App-header">
       <Select type="source" />
       <Select type="category" />
       <TextInput />
+      <button
+        type="button"
+        onClick={() => {
+          console.log("source ->", source);
+          console.log("category ->", category);
+          console.log("search ->", search);
+        }}
+      >
+        Search
+      </button>
     </header>
   );
 }
