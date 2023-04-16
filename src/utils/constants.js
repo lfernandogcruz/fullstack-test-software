d51be86b9f20175498cc449bf61db7c8b0336139
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-04-16 02:51:38 -0300

    feat: set options values for dinamic rendering

---
+++ src/utils/constants.js
@@ -0,0 +1,21 @@
+const categories = [
+  ['refrigerator', {'name': 'Geladeira'}],
+  ['tv', {'name': 'TV'}],
+  ['mobile', {'name': 'Celular'}],
+];
+
+const sources = [
+  ['all', {
+    'name': 'Todas',
+  }],
+  ['meli', {
+    'name': 'MercadoLivre',
+    'url': 'https://www.mercadolivre.com.br/',
+  }],
+  ['buscape', {
+    'name': 'Buscapé',
+    'url': 'https://www.buscape.com.br/',
+  }],
+];
+
+export { categories, sources };
