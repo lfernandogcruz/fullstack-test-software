f8ffbd4bc1b3ed1633bf71acbead8954d7b3e683
LuisCruz <lfernandogcruz@yahoo.com.br>
2023-04-16 02:43:16 -0300

    feat: create QueryProvider

---
+++ src/context/QueryProvider.js
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import QueryContext from './QueryContext';
+
+function QueryProvider({ children }) {
+  const [source, setSource] = useState('all');
+  const [category, setCategory] = useState('mobile');
+  const [search, setSearch] = useState('');
+  const [content, setContent] = useState([]);
+
+  const contextValue = {
+    source,
+    setSource,
+    category,
+    setCategory,
+    search,
+    setSearch,
+    content,
+    setContent,
+  };
+
+  return (
+    <QueryContext.Provider value={contextValue}>
+      {children}
+    </QueryContext.Provider>
+  );
+}
+
+QueryProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default QueryProvider;
